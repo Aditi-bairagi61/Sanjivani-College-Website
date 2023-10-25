8fde2092bba577e611d8087c4ac94adf1408ef7e
https://github.com/Aditi-bairagi61 <aditibairagi1456@gmail.com>
2023-10-25 21:41:43 +0530

    first commit

---
+++ admission.js
@@ -0,0 +1,14 @@
+function myfunc()
+{
+    let x=document.getElementById("first name").value;
+    let y=document.getElementById("last name").value;
+    let z=document.getElementById("email").value;
+    let a=document.getElementById("mobile").value;
+    let b=document.getElementById("address").value;
+    
+    let exp1=/^[a-z,0-9]+[@][a-z]+[.]+[a-z]+$/;
+    if(exp1.text(z))
+    {
+        alert("validation successful !")
+    }
+}
